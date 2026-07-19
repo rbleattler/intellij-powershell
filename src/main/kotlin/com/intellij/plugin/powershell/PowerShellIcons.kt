21368352e38f9db0664a909c379d94d60267d7b7
Friedrich von Never <friedrich@fornever.me>
2026-07-19 19:43:43 +0200

    (#175) New SVG icons
    Drawn myself, no third-party copyright anymore.

--- src/main/kotlin/com/intellij/plugin/powershell/PowerShellIcons.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/PowerShellIcons.kt
@@ -1,7 +1,9 @@
-// SPDX-FileCopyrightText: 2017 Andrey Dernov <https://github.com/ant-druha/>
-// SPDX-FileCopyrightText: 2023 intellij-powershell contributors <https://github.com/intellij-powershell/intellij-powershell>
-//
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * SPDX-FileCopyrightText: 2017 Andrey Dernov <https://github.com/ant-druha/>
+ * SPDX-FileCopyrightText: 2023-2026 intellij-powershell contributors <https://github.com/intellij-powershell/intellij-powershell>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package com.intellij.plugin.powershell
 
@@ -12,5 +14,5 @@ import com.intellij.openapi.util.IconLoader
  * Andrey 17/07/17.
  */
 object PowerShellIcons {
-  val FILE = IconLoader.getIcon("/icons/powershell_icon.png", PowerShellIcons.javaClass)
+  val FILE = IconLoader.getIcon("/icons/powershell_icon.svg", PowerShellIcons.javaClass)
 }
